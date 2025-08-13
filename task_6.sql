83cddfff34e31fa77fb276f096f8f6a72af7db72
MubarekAdem <mubarekadem001@gmail.com>
2025-08-13 16:57:53 +0300

    task 6

--- task_6.sql
+++ task_6.sql
@@ -1,2 +1,6 @@
-INSERT INTO customer (customer_id, first_name, last_name, email, address)
-VALUES (2, 'SomeFirstName', 'SomeLastName', 'someemail@example.com', '124 Happiness  Ave.');
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
