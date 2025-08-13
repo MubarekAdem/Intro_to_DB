83d2af9b42a92dabee3b5ca9793db5ac439741b1
MubarekAdem <mubarekadem001@gmail.com>
2025-08-13 16:53:22 +0300

    task 5

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,4 @@
--- Insert a single customer into the Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO customer (column1, column2, column3)
+VALUES
+    ('value1', 'value2', 'value3'),
+    ('value4', 'value5', 'value6');
